9b6a3ff4eca31404d1713ddadeb422a3a251cbbd
Daehyeon <daehyeon1735@gmail.com>
2024-07-12 16:19:22 +0900

    Admin 생성

---
+++ src/main/java/vendingMachineHero/admin/dao/AdminDAO.java
@@ -0,0 +1,10 @@
+package vendingMachineHero.admin.dao;
+
+import java.util.ArrayList;
+
+public class AdminDAO {
+	
+	public static AdminDAO instance = new AdminDAO();
+	
+
+}
--- src/main/java/vendingMachineHero/dao/AdminDAO.java
+++
@@ -1,5 +0,0 @@
-package vendingMachineHero.dao;
-
-public class AdminDAO {
-	
-}
