082499e57015548d330c88286e6867158217fc46
ohwoong2 <ohwoonge@gmail.com>
2024-07-12 14:04:13 +0900

    Admin DAO

---
+++ src/main/java/vendingMachineHero/admin/dao/UserDAO.java
@@ -0,0 +1,50 @@
+package vendingMachineHero.admin.dao;
+
+import java.util.ArrayList;
+
+import vendingMachineHero.model.User;
+
+public class UserDAO {
+	public static UserDAO instance = new UserDAO();
+	
+	private ArrayList<User> users = new ArrayList<>();
+	
+	// 모든 유저 검색
+		public ArrayList<User> showUsers(){
+			return users;	
+		}
+		// 특정 유저 검색
+		public User getUser(String username) {
+			for (User u : users) {
+				if (u != null && u.getUsername().equals(username) ) {
+					return u;
+				}
+			}
+			return null;
+				
+		}
+		// 새로운 유저 저장
+		public User saveUser(String username){
+			User u = getUser(username);
+			if (u != null) {
+				System.out.println("해당 user는 이미 존재합니다."); 
+				return null;
+
+			}
+			else {
+				User newUser = new User(username);
+				users.add(newUser);
+				System.out.println(newUser.getUsername() +" 생성 완료, 리스트에 저장합니다.");
+				return u;
+			}
+			
+		}
+		
+		public void deleteUser(String username){
+			User u = getUser(username);
+			if (u != null && users.contains(u)) {
+				System.out.println();
+			}
+			
+		}
+}
---
+++ src/test/java/step04_miniProject/Test.java
@@ -0,0 +1,18 @@
+package step04_miniProject;
+
+import vendingMachineHero.admin.dao.UserDAO;
+
+public class Test {
+
+	@org.junit.Test
+	public void t1() {
+		UserDAO u = new UserDAO();
+		u.saveUser("dffdg");
+		System.out.println(u.getUser("dffdg"));;
+		u.getUser("dff22dg");
+		u.showUsers();
+		u.deleteUser("dffdg");
+		u.showUsers();
+
+	}
+}
